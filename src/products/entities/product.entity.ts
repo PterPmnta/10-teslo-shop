56f286e2454ef6c55bd94b3844794a0c1a4768a8
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-12-18 13:13:20 -0500

    clase 127 - entidad sin relaciones

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -5,4 +5,16 @@ export class Product {
     @PrimaryGeneratedColumn('uuid') id: number;
 
     @Column('text', { unique: true }) title: string;
+
+    @Column('numeric', { default: 0 }) price: number;
+
+    @Column({ type: 'text', nullable: true }) description: string;
+
+    @Column({ type: 'text', unique: true }) slug: string;
+
+    @Column({ type: 'int', default: 0 }) stock: number;
+
+    @Column({ type: 'text', array: true }) sizes: string[];
+
+    @Column({ type: 'text' }) gender: string;
 }
