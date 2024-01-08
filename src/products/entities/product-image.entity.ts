eaa26604ebce832c8003ae4b583a5912ece164bc
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2024-01-08 12:45:17 -0500

    clase 145 - onetomany & manytoone

--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -1,8 +1,14 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Product } from './product.entity';
 
 @Entity('product-image')
 export class ProductImage {
     @PrimaryGeneratedColumn('uuid') id: string;
 
     @Column('text', { array: true }) url: string[];
+
+    @ManyToOne(() => Product, (product) => product.images, {
+        onDelete: 'CASCADE',
+    })
+    product: Product;
 }
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,8 +3,10 @@ import {
     BeforeUpdate,
     Column,
     Entity,
+    OneToMany,
     PrimaryGeneratedColumn,
 } from 'typeorm';
+import { ProductImage } from './product-image.entity';
 
 @Entity('product')
 export class Product {
@@ -26,6 +28,11 @@ export class Product {
 
     @Column({ type: 'text', array: true, default: [], nullable: true })
     tags: string[];
+
+    @OneToMany(() => ProductImage, (productImage) => productImage.product, {
+        cascade: true,
+    })
+    images?: ProductImage;
 
     @BeforeInsert()
     checkSlugInsert() {
