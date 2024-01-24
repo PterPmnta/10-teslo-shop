e1bd3bac1731d147fa696aa1730fe3ebeb95a862
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2024-01-23 19:57:59 -0500

    clase 177 - jwtstrategy - parte 2

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,19 +1,41 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { User } from '../entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { IJwtPayload } from '../interfaces/jwt-payload.interface';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor() {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+        configService: ConfigService,
+    ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,
-            secretOrKey: process.env.JWT_SECRET,
+            //ignoreExpiration: false,
+            secretOrKey: configService.get('JWT_SECRET'),
         });
     }
 
     async validate(payload: IJwtPayload): Promise<User> {
         const { email } = payload;
-        return;
+
+        const user = await this.userRepository.findOneBy({ email });
+
+        if (!user) {
+            throw new UnauthorizedException('Token not valid.');
+        }
+
+        if (!user.isActive) {
+            throw new UnauthorizedException(
+                'User is not active. Talk to the admin.',
+            );
+        }
+
+        return user;
     }
 }
