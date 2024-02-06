fe5e8f2084a6904f8917e11136dec7e2e2032d2f
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2024-02-06 16:06:38 -0500

    clase 208 - server - escuchar conexiones y desconecciones

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,22 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    WebSocketGateway,
+} from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
+export class MessagesWsGateway
+    implements OnGatewayConnection, OnGatewayDisconnect
+{
     constructor(private readonly messagesWsService: MessagesWsService) {}
+    handleConnection(client: Socket) {
+        //throw new Error('Method not implemented.');
+        console.log('Cliente conectado', client.id);
+    }
+    handleDisconnect(client: any) {
+        //throw new Error('Method not implemented.');
+        console.log('Cliente desconectado', client.id);
+    }
 }
