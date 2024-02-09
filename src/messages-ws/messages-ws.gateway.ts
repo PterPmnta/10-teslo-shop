75e6186753ee9624805ae3f77fe78bd8949309bb
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2024-02-08 20:35:11 -0500

    clase 212 - cliente - clientes conectados

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,21 +2,32 @@ import {
     OnGatewayConnection,
     OnGatewayDisconnect,
     WebSocketGateway,
+    WebSocketServer,
 } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
     implements OnGatewayConnection, OnGatewayDisconnect
 {
+    @WebSocketServer() wss: Server;
+
     constructor(private readonly messagesWsService: MessagesWsService) {}
 
     handleConnection(client: Socket) {
         this.messagesWsService.registerClient(client);
+        this.wss.emit(
+            'clients-updated',
+            this.messagesWsService.getConnectedClients(),
+        );
     }
 
     handleDisconnect(client: any) {
         this.messagesWsService.removeClient(client.id);
+        this.wss.emit(
+            'clients-updated',
+            this.messagesWsService.getConnectedClients(),
+        );
     }
 }
